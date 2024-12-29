32f45e60c372e53a72b3215e9401e276ee591161
hasan <hasan-ha@ba-tech.tech>
2024-12-29 10:03:30 +0300

    added my hotels view and added e2e test

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,3 +1,4 @@
+import { HotelType } from "./../../backend/src/shared/types";
 import { RegisterFormData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
 
@@ -73,6 +74,18 @@ export const addMyHotel = async (hotelFormData: FormData) => {
 
   if (!response.ok) {
     throw new Error("Failed to add hotel");
+  }
+
+  return response.json();
+};
+
+export const fetchMyHotels = async (): Promise<HotelType[]> => {
+  const response = await fetch(`${API_BASE_URL}/api/my-hotels`, {
+    credentials: "include",
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch hotels");
   }
 
   return response.json();
